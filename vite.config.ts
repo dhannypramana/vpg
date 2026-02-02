ea00b341492513f2b58aef180d8d9b6f6186eb87
Dhanny Adhi Pramana <dhanny.pramana@nostratech.com>
2026-02-02 12:17:58 +0700

    chore(vite): setup vite config env port

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,30 @@
-import { fileURLToPath, URL } from 'node:url';
+import path from 'node:path';
+import Vue from '@vitejs/plugin-vue';
+import { defineConfig, loadEnv } from 'vite';
+import VueDevTools from 'vite-plugin-vue-devtools';
 
-import vue from '@vitejs/plugin-vue';
-import { defineConfig } from 'vite';
-import vueDevTools from 'vite-plugin-vue-devtools';
+export default defineConfig(({ mode }) => {
+    const env = { ...process.env, ...loadEnv(mode, `${process.cwd()}`, '') };
+    const port = !Number.isNaN(Number(env.VITE_APP_PORT)) ? Number(env.VITE_APP_PORT) : 8002;
 
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [
-        vue(),
-        vueDevTools(),
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
+    return {
+        plugins: [
+            Vue(),
+            VueDevTools(),
+        ],
+
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'src'),
+                '@root': path.resolve(__dirname),
+                '@common': path.resolve(__dirname, 'src/common'),
+                '@features': path.resolve(__dirname, 'src/features'),
+            },
         },
-    },
+
+        server: {
+            port,
+            strictPort: true,
+        },
+    };
 });
